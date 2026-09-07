f1cd65e378778001ad53930da3d2f8756f2f40ed
Илья <irotgan777@mail.ru>
2026-09-07 22:47:42 +0500

    task5

--- task05/src/main/java/com/example/task05/Task05Main.java
+++ task05/src/main/java/com/example/task05/Task05Main.java
@@ -3,10 +3,16 @@ package com.example.task05;
 public class Task05Main {
 
     public static String solution(int x) {
+        if (x == 0) {return "TRUE";}
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x > 0){
+            int digit = x % 10;
+            if (digit % 2 != 0) {
+                return "FALSE";
+            }
+            x /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
