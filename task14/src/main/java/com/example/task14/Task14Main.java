580b547a36e3accea26cebae99072adb4d57725d
Илья <irotgan777@mail.ru>
2026-09-07 23:44:55 +0500

    task14

--- task14/src/main/java/com/example/task14/Task14Main.java
+++ task14/src/main/java/com/example/task14/Task14Main.java
@@ -5,9 +5,9 @@ public class Task14Main {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
+        return Integer.parseInt(new StringBuilder(Integer.toString(value)).reverse().toString());
 
-        return 0;
+
     }
 
     public static void main(String[] args) {
